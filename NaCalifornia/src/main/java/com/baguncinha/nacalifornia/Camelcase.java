7da5823745b1911f261e19249370f1ae7b92d243
TraPxEziO <lucasazevedo0403@gmail.com>
2018-06-26 10:11:17 -0300

    koe

---
+++ NaCalifornia/src/main/java/com/baguncinha/nacalifornia/Camelcase.java
@@ -0,0 +1,18 @@
+package com.baguncinha.nacalifornia;
+
+import java.util.Scanner;
+
+public class Camelcase {
+
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;       
+        System.out.println("O resultado da conta eh:\n" + (x*y));
+        String palavra = "Legal", frase = "Java eh:\n"+palavra;
+        System.out.println(frase); 
+        for (int i = 0; i <=10; i++){
+            System.out.println("");
+        }
+    }
+}
